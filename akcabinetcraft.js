c72519bcc8289f39dca7a9ca7522e99bb778be12
Evgeny2723 <96776434+Evgeny2723@users.noreply.github.com>
2025-09-29 09:46:30 +0500

    Update akcabinetcraft.js

--- akcabinetcraft.js
+++ akcabinetcraft.js
@@ -323,7 +323,14 @@ $("#cta-form, #lp-target-form").validate({
   highlight: function(element) { $(element).css("border-bottom", "1px solid var(--error)"); },
   unhighlight: function(element) { $(element).css("border-bottom", "1px solid var(--white)"); },
 });
-$("#corp-cta-form, #new-cta-form").validate({
+$("#corp-cta-form").validate({
+  rules: { firstname: { required: true }, lastname: { required: true }, phone: { required: true, phoneUS_complete: true }, email: { required: true, email_strict: true }, 'project-type': { required: true } },
+  messages: { email: { email: "Invalid email" }, 'project-type': { required: "Please select a project type from the list to proceed." } },
+  errorPlacement: function(error, element) { error.appendTo(element.closest(".input-wrapper")); },
+  highlight: function(element) { $(element).css("background-color", "rgba(200, 15, 15, 0.06)").addClass("error-placeholder"); },
+  unhighlight: function(element) { $(element).css("background-color", "").removeClass("error-placeholder"); },
+});
+  $("#new-cta-form").validate({
   rules: { firstname: { required: true }, lastname: { required: true }, phone: { required: true, phoneUS_complete: true }, email: { required: true, email_strict: true }, 'project-type': { required: true } },
   messages: { email: { email: "Invalid email" }, 'project-type': { required: "Please select a project type from the list to proceed." } },
   errorPlacement: function(error, element) { error.appendTo(element.closest(".input-wrapper")); },
